bd7176d38e002dbce7caa68af429b75c52fbc71b
Neeraj Dalal <nd941z@gmail.com>
2021-04-15 15:20:33 +0530

    Thu Apr 15 09:50:33 UTC 2021

---
+++ hub.sh
@@ -0,0 +1,32 @@
+hub() {
+data=$(cat<<DATA
+usage:
+  clone <username/reponame> | <reponame>
+  create (optional reponame)
+  list
+  login
+  logout
+  push
+  status
+DATA
+)
+  if [ $# = 1 ] ; then
+    case $1 in
+      create) git init ; gh repo create $(basename `pwd`) -d '' --private -y ;;
+      list) gh repo list ;;
+      login) gh auth login -w ;;
+      logout) gh auth logout ;;
+      push) git add -A ; git commit -m "`date -u`" ; git push ;;
+      status) gh auth status ;;
+      *) echo $data ; false ;;
+    esac
+  elif [ $# = 2 ] ; then
+    case $1 in
+      clone) gh repo clone $2 ;;
+      create) git init ; gh repo create $2 -d '' --private -y ;;
+      *) echo $data ; false ;;
+    esac
+  else
+    echo $data ; false
+  fi
+}
